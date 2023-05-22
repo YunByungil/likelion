838a06dba6a83fe5ac516619f9d361fea84360f3
YunByungil <dbsquddlfz@naver.com>
2023-05-22 23:14:42 +0900

    feat: Controller, 비밀번호 변경 Api 구현

--- src/main/java/com/likelion/apicontroller/UserApiController.java
+++ src/main/java/com/likelion/apicontroller/UserApiController.java
@@ -1,11 +1,11 @@
 package com.likelion.apicontroller;
 
 import com.likelion.dto.user.UserJoinRequestDto;
+import com.likelion.dto.user.UserUpdateDto;
 import com.likelion.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -16,5 +16,11 @@ public class UserApiController {
     @PostMapping("/api/v1/join")
     public Long save(@RequestBody UserJoinRequestDto requestDto) {
         return userService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/user/{id}")
+    public Long updateUserInfo(@PathVariable Long id,
+                               @RequestBody UserUpdateDto requestDto) {
+        return userService.update(id, requestDto);
     }
 }
