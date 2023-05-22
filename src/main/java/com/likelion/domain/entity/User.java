0219848b3fc5ec3e3667df62a882a5dd4dec4240
YunByungil <dbsquddlfz@naver.com>
2023-05-22 21:33:41 +0900

    feat: User Entity 생성

---
+++ src/main/java/com/likelion/domain/entity/User.java
@@ -0,0 +1,33 @@
+package com.likelion.domain.entity;
+
+import com.likelion.domain.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+
+    private String userName;
+
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    @Builder
+    public User(String userName, String password, UserRole role) {
+        this.userName = userName;
+        this.password = password;
+        this.role = role;
+    }
+}
---
+++ src/main/java/com/likelion/domain/enums/UserRole.java
@@ -0,0 +1,5 @@
+package com.likelion.domain.enums;
+
+public enum UserRole {
+    USER, ADMIN
+}
