2d66d535a5f1e693d2cba1ec1a9a820ed57df076
YunByungil <dbsquddlfz@naver.com>
2023-05-24 00:29:52 +0900

    feat: 게시글 목록 조회 Api 구현

--- src/main/java/com/likelion/apicontroller/PostApiController.java
+++ src/main/java/com/likelion/apicontroller/PostApiController.java
@@ -1,14 +1,19 @@
 package com.likelion.apicontroller;
 
 import com.likelion.domain.entity.Post;
+import com.likelion.dto.post.PostResponseDto;
 import com.likelion.dto.post.PostSaveRequestDto;
 import com.likelion.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @RestController
@@ -21,6 +26,15 @@ public class PostApiController {
         Post savedPost = postService.save(requestDto);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(savedPost);
+    }
+
+    @GetMapping("/api/v1/post")
+    public ResponseEntity<List<PostResponseDto>> getAllPost() {
+        List<PostResponseDto> postList = postService.findAll().stream()
+                .map(PostResponseDto::new)
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok().body(postList);
     }
 
 }
