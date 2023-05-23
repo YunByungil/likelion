0116e718980b7111818f2953d8e248e0015a315b
YunByungil <dbsquddlfz@naver.com>
2023-05-23 23:38:58 +0900

    feat: PostEntity 생성

---
+++ src/main/java/com/likelion/domain/entity/Post.java
@@ -0,0 +1,29 @@
+package com.likelion.domain.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String content;
+
+    @Builder
+    public Post(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
