cfe20dd159177f7971a1760055c8ce605e5a28c2
YunByungil <dbsquddlfz@naver.com>
2023-05-24 01:34:44 +0900

    feat: PostService, 게시글 상세 조회 기능 구현

--- src/main/java/com/likelion/service/PostService.java
+++ src/main/java/com/likelion/service/PostService.java
@@ -2,6 +2,7 @@ package com.likelion.service;
 
 import com.likelion.domain.entity.Post;
 import com.likelion.domain.repository.PostRepository;
+import com.likelion.dto.post.PostResponseDto;
 import com.likelion.dto.post.PostSaveRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -20,7 +21,16 @@ public class PostService {
         return postRepository.save(requestDto.toEntity());
     }
 
+    // TODO: List<PostResponseDto>로 수정
     public List<Post> findAll() {
         return postRepository.findAll();
+    }
+
+    // TODO: PostListResponse, PostResponse 두 개 나누기
+    public PostResponseDto findById(Long id) {
+        Post post = postRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 게시글입니다."));
+
+        return new PostResponseDto(post);
     }
 }
