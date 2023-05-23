ca0899926b65c7a371ab31d70b80030cc9d4ac68
YunByungil <dbsquddlfz@naver.com>
2023-05-24 00:10:24 +0900

    test: 게시글 저장 Controller 테스트 작성

---
+++ src/test/java/com/likelion/apicontroller/PostApiControllerTest.java
@@ -0,0 +1,77 @@
+package com.likelion.apicontroller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.likelion.domain.entity.Post;
+import com.likelion.domain.repository.PostRepository;
+import com.likelion.dto.post.PostSaveRequestDto;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+class PostApiControllerTest {
+
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mvc;
+
+    @Autowired
+    PostRepository postRepository;
+
+    @BeforeEach
+    public void setUp() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .build();
+    }
+
+    @AfterEach
+    public void end() {
+        postRepository.deleteAll();
+    }
+
+    @DisplayName("게시글 작성 성공")
+    @Test
+    void addPost() throws Exception {
+        // given
+        String title = "제목";
+        String content = "내용";
+
+        PostSaveRequestDto requestDto = PostSaveRequestDto.builder()
+                .title(title)
+                .content(content)
+                .build();
+
+
+        String url = "http://localhost:8080/api/v1/post";
+
+        // when
+        mvc.perform(post(url)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(requestDto)))
+                .andExpect(status().isCreated());
+
+        // then
+        List<Post> all = postRepository.findAll();
+        assertThat(all.get(0).getTitle()).isEqualTo(title);
+        assertThat(all.get(0).getContent()).isEqualTo(content);
+
+    }
+}
