af6d761623d63075dc3111218a2f4457b7105b50
YunByungil <dbsquddlfz@naver.com>
2023-05-26 00:49:42 +0900

    feat: 게시글 수정/생성 컨트롤러 기능 구현

--- src/main/java/com/likelion/controller/PostController.java
+++ src/main/java/com/likelion/controller/PostController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -33,5 +34,18 @@ public class PostController {
         model.addAttribute("post", post);
 
         return "post";
+    }
+
+    @GetMapping("/new-post")
+    public String newPost(@RequestParam(required = false) Long id,
+                          Model model) {
+        if (id == null) {
+            model.addAttribute("post", new PostResponseDto());
+        } else {
+            PostResponseDto post = postService.findById(id);
+            model.addAttribute("post", post);
+        }
+
+        return "newPost";
     }
 }
