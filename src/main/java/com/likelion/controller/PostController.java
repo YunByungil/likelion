0fa4c7bad997b7037625372c6c38b63a11a9d5e7
YunByungil <dbsquddlfz@naver.com>
2023-05-25 22:46:54 +0900

    feat: PostController, 게시글 상세 페이지 기능 구현
    - 게시글 상세보기 메서드 구현

--- src/main/java/com/likelion/controller/PostController.java
+++ src/main/java/com/likelion/controller/PostController.java
@@ -2,11 +2,13 @@ package com.likelion.controller;
 
 import com.likelion.domain.entity.Post;
 import com.likelion.dto.post.PostListResponseDto;
+import com.likelion.dto.post.PostResponseDto;
 import com.likelion.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -26,5 +28,13 @@ public class PostController {
 
         model.addAttribute("postList", postList);
         return "postList";
+    }
+
+    @GetMapping("/post/{id}")
+    public String getPost(@PathVariable Long id, Model model) {
+        PostResponseDto post = postService.findById(id);
+        model.addAttribute("post", post);
+
+        return "post";
     }
 }
