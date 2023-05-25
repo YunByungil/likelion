a3c8afb66c2470b65906e1744864d6fd83ab1474
YunByungil <dbsquddlfz@naver.com>
2023-05-25 19:57:08 +0900

    refactor: 처음 생성되는 값 createdAt 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-insert into post (title, content) values ('제목1', '내용1')
-insert into post (title, content) values ('제목2', '내용2')
-insert into post (title, content) values ('제목3', '내용3')
+insert into post (title, content, createdAt, updatedAt, deletedAt) values ('제목1', '내용1', NOW(), NOW(), null)
+insert into post (title, content, createdAt, updatedAt, deletedAt) values ('제목2', '내용2', NOW(), NOW(), null)
+insert into post (title, content, createdAt, updatedAt, deletedAt) values ('제목3', '내용3', NOW(), NOW(), null)
