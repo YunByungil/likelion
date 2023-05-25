64a619c17f2ad97891b2c1ba2a7ad92557265779
YunByungil <dbsquddlfz@naver.com>
2023-05-25 19:59:56 +0900

    fix: 컬럼명 잘못 적은 거 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-insert into post (title, content, createdAt, updatedAt, deletedAt) values ('제목1', '내용1', NOW(), NOW(), null)
-insert into post (title, content, createdAt, updatedAt, deletedAt) values ('제목2', '내용2', NOW(), NOW(), null)
-insert into post (title, content, createdAt, updatedAt, deletedAt) values ('제목3', '내용3', NOW(), NOW(), null)
+insert into post (title, content, created_At, updated_At, deleted_At) values ('제목1', '내용1', NOW(), NOW(), null)
+insert into post (title, content, created_At, updated_At, deleted_At) values ('제목2', '내용2', NOW(), NOW(), null)
+insert into post (title, content, created_At, updated_At, deleted_At) values ('제목3', '내용3', NOW(), NOW(), null)
