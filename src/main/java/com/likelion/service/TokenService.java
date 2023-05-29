747e77e49a24010075701fdbcb69996f121928a4
YunByungil <dbsquddlfz@naver.com>
2023-05-29 18:50:48 +0900

    feat: CreateAccessToken 기능 구현

---
+++ src/main/java/com/likelion/service/TokenService.java
@@ -0,0 +1,28 @@
+package com.likelion.service;
+
+import com.likelion.config.jwt.TokenProvider;
+import com.likelion.domain.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+
+@RequiredArgsConstructor
+@Service
+public class TokenService {
+
+    private final TokenProvider tokenProvider;
+    private final RefreshTokenService refreshTokenService;
+    private final UserService userService;
+
+    public String createNewAccessToken(String refreshToken) {
+        if (!tokenProvider.validToken(refreshToken)) {
+            throw new IllegalArgumentException("존재하지 않는 토큰입니다.");
+        }
+
+        Long userId = refreshTokenService.findByRefreshToken(refreshToken).getUserId();
+        User user = userService.findById(userId);
+
+        return tokenProvider.generateToken(user, Duration.ofHours(2));
+    }
+}
