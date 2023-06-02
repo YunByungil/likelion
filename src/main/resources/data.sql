2563480a7660952daebde45e9dd86fd941646fc2
YunByungil <dbsquddlfz@naver.com>
2023-06-02 18:06:03 +0900

    feat: data.sql author 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-insert into post (title, content, created_At, updated_At, deleted_At) values ('제목1', '내용1', NOW(), NOW(), null)
-insert into post (title, content, created_At, updated_At, deleted_At) values ('제목2', '내용2', NOW(), NOW(), null)
-insert into post (title, content, created_At, updated_At, deleted_At) values ('제목3', '내용3', NOW(), NOW(), null)
+insert into post (title, content, author, created_At, updated_At, deleted_At) values ('제목1', '내용1', 'user1', NOW(), NOW(), null)
+insert into post (title, content, author, created_At, updated_At, deleted_At) values ('제목2', '내용2', 'user2', NOW(), NOW(), null)
+insert into post (title, content, author, created_At, updated_At, deleted_At) values ('제목3', '내용3', 'user3', NOW(), NOW(), null)
