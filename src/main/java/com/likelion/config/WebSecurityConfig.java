7b6f0df81403c714950ce5eecf95209460feb8a1
YunByungil <dbsquddlfz@naver.com>
2023-06-02 18:17:45 +0900

    feat: OAuth2에 맞는 SecurityConfig 작성

--- src/main/java/com/likelion/config/WebSecurityConfig.java
+++ src/main/java/com/likelion/config/WebSecurityConfig.java
@@ -1,23 +1,41 @@
 package com.likelion.config;
 
+import com.likelion.config.jwt.TokenProvider;
+import com.likelion.config.oauth.OAuth2AuthorizationRequestBasedOnCookieRepository;
+import com.likelion.config.oauth.OAuth2SuccessHandler;
+import com.likelion.config.oauth.OAuth2UserCustomService;
+import com.likelion.domain.enums.UserRole;
+import com.likelion.domain.repository.RefreshTokenRepository;
 import com.likelion.service.UserDetailService;
+import com.likelion.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 
 @RequiredArgsConstructor
 @Configuration
+@Slf4j
 public class WebSecurityConfig {
 
-    private final UserDetailService detailService;
+    private final OAuth2UserCustomService oAuth2UserCustomService;
+    private final RefreshTokenRepository refreshTokenRepository;
+//    private final UserDetailService detailService;
+    private final UserService userService;
+    private final TokenProvider tokenProvider;
 
     @Bean
     public WebSecurityCustomizer configure() {
@@ -27,36 +45,90 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http
-                .authorizeHttpRequests()
-                .requestMatchers("/login", "/join", "/user", "/h2-console/**", "/api/v1/post", "api/v1/join").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/login")
-                .defaultSuccessUrl("/post")
-                .and()
-                .logout()
-                .logoutSuccessUrl("/login")
-                .invalidateHttpSession(true)
-                .and()
-                .csrf().disable()
-                .build();
+        System.out.println("\"gdgdgd111\" = " + "gdgdgd111");
+        http.csrf().disable()
+                .httpBasic().disable()
+                .formLogin().disable()
+                .logout().disable();
+
+        http.sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        http.authorizeHttpRequests()
+                .requestMatchers("/api/v1/token", "/api/v1/join", "/api/v1/login").permitAll()
+                .requestMatchers("/api/v1/post").hasRole("USER")
+                .requestMatchers("/api/v1/**").authenticated()
+                .anyRequest().permitAll();
+
+        http.oauth2Login()
+                        .loginPage("/login")
+                        .authorizationEndpoint()
+                        .authorizationRequestRepository(oAuth2AuthorizationRequestBasedOnCookieRepository())
+                        .and()
+                        .successHandler(oAuth2SuccessHandler())
+                        .userInfoEndpoint()
+                        .userService(oAuth2UserCustomService);
+
+        http.logout()
+                .logoutSuccessUrl("/login");
+
+        http.exceptionHandling()
+                .defaultAuthenticationEntryPointFor(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED),
+                         new AntPathRequestMatcher("/api/v1/**"));
+        return http.build();
+
+
+
+//        return http
+//                .authorizeHttpRequests()
+//                .requestMatchers("/login", "/join", "/user", "/h2-console/**", "/api/v1/post", "api/v1/join").permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//                .formLogin()
+//                .loginPage("/login")
+//                .defaultSuccessUrl("/post")
+//                .and()
+//                .logout()
+//                .logoutSuccessUrl("/login")
+//                .invalidateHttpSession(true)
+//                .and()
+//                .csrf().disable()
+//                .build();
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(HttpSecurity http,
-                                                       BCryptPasswordEncoder bCryptPasswordEncoder,
-                                                       UserDetailService userDetailService) throws Exception {
-        return http.getSharedObject(AuthenticationManagerBuilder.class)
-                .userDetailsService(detailService)
-                .passwordEncoder(bCryptPasswordEncoder)
-                .and()
-                .build();
+    public OAuth2SuccessHandler oAuth2SuccessHandler() {
+        return new OAuth2SuccessHandler(tokenProvider,
+                refreshTokenRepository,
+                oAuth2AuthorizationRequestBasedOnCookieRepository(),
+                userService);
     }
 
     @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
+    public OAuth2AuthorizationRequestBasedOnCookieRepository oAuth2AuthorizationRequestBasedOnCookieRepository() {
+        return new OAuth2AuthorizationRequestBasedOnCookieRepository();
     }
+
+//    @Bean
+//    public AuthenticationManager authenticationManager(HttpSecurity http,
+//                                                       BCryptPasswordEncoder bCryptPasswordEncoder,
+//                                                       UserDetailService userDetailService) throws Exception {
+//        return http.getSharedObject(AuthenticationManagerBuilder.class)
+//                .userDetailsService(detailService)
+//                .passwordEncoder(bCryptPasswordEncoder)
+//                .and()
+//                .build();
+//    }
+
+    @Bean
+    public TokenAuthenticationFilter tokenAuthenticationFilter() {
+        return new TokenAuthenticationFilter(tokenProvider);
+    }
+
+//    @Bean
+//    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
 }
