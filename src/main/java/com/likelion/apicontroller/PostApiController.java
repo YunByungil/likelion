159781306c1c777ce129b8ace2308b8056a3a771
YunByungil <dbsquddlfz@naver.com>
2023-06-02 18:10:45 +0900

    feat: 글쓴이 정보 추가 구현

--- src/main/java/com/likelion/apicontroller/PostApiController.java
+++ src/main/java/com/likelion/apicontroller/PostApiController.java
@@ -1,6 +1,7 @@
 package com.likelion.apicontroller;
 
 import com.likelion.domain.entity.Post;
+import com.likelion.domain.entity.User;
 import com.likelion.dto.post.PostListResponseDto;
 import com.likelion.dto.post.PostResponseDto;
 import com.likelion.dto.post.PostSaveRequestDto;
@@ -9,8 +10,10 @@ import com.likelion.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -21,8 +24,9 @@ public class PostApiController {
     private final PostService postService;
 
     @PostMapping("/api/v1/post")
-    public ResponseEntity<Post> addPost(@RequestBody PostSaveRequestDto requestDto) {
-        Post savedPost = postService.save(requestDto);
+    public ResponseEntity<Post> addPost(@RequestBody PostSaveRequestDto requestDto,
+                                        Principal principal) {
+        Post savedPost = postService.save(requestDto, principal.getName());
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(savedPost);
     }
