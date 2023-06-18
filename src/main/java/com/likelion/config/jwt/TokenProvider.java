2dca863d595a928d36953e7ff1ea37a67813655a
YunByungil <dbsquddlfz@naver.com>
2023-06-18 15:37:27 +0900

    refactor: 권한 주는 로직 ROLE_USER 고정 -> 로그인 값으로 수정

--- src/main/java/com/likelion/config/jwt/TokenProvider.java
+++ src/main/java/com/likelion/config/jwt/TokenProvider.java
@@ -2,11 +2,13 @@ package com.likelion.config.jwt;
 
 import com.likelion.config.JwtProperties;
 import com.likelion.domain.entity.User;
+import com.likelion.domain.enums.UserRole;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Header;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -20,6 +22,7 @@ import java.util.Set;
 
 @RequiredArgsConstructor
 @Service
+@Slf4j
 public class TokenProvider {
 
     private final JwtProperties jwtProperties;
@@ -34,6 +37,7 @@ public class TokenProvider {
         // TODO: SecretKey 못 받는 에러 잡아야 됨. -> test 전용 yml에 추가를 안 해서 발생한 에러...!
         System.out.println("jwtProperties = " + jwtProperties.getIssuer());
         System.out.println("jwtProperties = " + jwtProperties.getSecretKey());
+        log.info("userRole check = {} ", user.getRole());
         return Jwts.builder()
                 .setHeaderParam(Header.TYPE, Header.JWT_TYPE) // 헤더 typ : JWT
                 .setIssuer(jwtProperties.getIssuer()) // 내용 iss : properties 파일에서 설정한 값
@@ -41,6 +45,7 @@ public class TokenProvider {
                 .setExpiration(expiry) // 내용 exp : expiry 멤버 변수값
                 .setSubject(user.getUsername()) // 내용 sub : 유저 계정
                 .claim("id", user.getId()) // 클레임 id : 유저 ID
+                .claim("role", user.getRole())
                 .signWith(SignatureAlgorithm.HS256, jwtProperties.getSecretKey())
                 .compact();
     }
@@ -56,10 +61,14 @@ public class TokenProvider {
         }
     }
 
+    /**
+     * makeToken() 메서드에서 claim("role") 권한을 넣어놓고,
+     * getClaims() 메서드를 이용해서 유저 권한을 빼자.
+     */
     public Authentication getAuthentication(String token) {
         Claims claims = getClaims(token);
         Set<SimpleGrantedAuthority> authorities =
-                Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"));
+                Collections.singleton(new SimpleGrantedAuthority("ROLE_" + claims.get("role")));
 
         return new UsernamePasswordAuthenticationToken(
                 new org.springframework.security.core.userdetails.User(
@@ -71,6 +80,7 @@ public class TokenProvider {
         Claims claims = getClaims(token);
         return claims.get("id", Long.class);
     }
+
 
     private Claims getClaims(String token) {
         return Jwts.parser()
