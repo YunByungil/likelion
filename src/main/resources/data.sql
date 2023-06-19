0d0d254d6298ddc28bfeed109679815551a1f377
YunByungil <dbsquddlfz@naver.com>
2023-06-19 21:45:24 +0900

    refactor: data.sql 구조 변경 및 postEntity 값 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-insert into post (title, content, author, created_At, updated_At, deleted_At) values ('제목1', '내용1', 'user1', NOW(), NOW(), null)
-insert into post (title, content, author, created_At, updated_At, deleted_At) values ('제목2', '내용2', 'user2', NOW(), NOW(), null)
-insert into post (title, content, author, created_At, updated_At, deleted_At) values ('제목3', '내용3', 'user3', NOW(), NOW(), null)
-insert into users (email, nickname, password, role, username, created_At, updated_At, deleted_At) values ('test@naver.com', '테스트1', '1234', 'user', '테스트닉넹미1', NOW(), NOW(), null)
+insert into users (email, nickname, password, role, username, created_At, updated_At, deleted_At) values ('test@naver.com', '테스트1', '1234', 'USER', '테스트닉넹미1', NOW(), NOW(), null)
+insert into post (title, content, author, created_At, updated_At, deleted_At, user_id) values ('제목1', '내용1', 'user1', NOW(), NOW(), null, 1)
+insert into post (title, content, author, created_At, updated_At, deleted_At, user_id) values ('제목2', '내용2', 'user2', NOW(), NOW(), null, 1)
+insert into post (title, content, author, created_At, updated_At, deleted_At, user_id) values ('제목3', '내용3', 'user3', NOW(), NOW(), null, 1)
